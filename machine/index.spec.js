c5935d5a3fba23d74efa38af0264daddc5ccb9de
Maciej Smolinski <contact@maciejsmolinski.com>
2022-02-01 23:39:59 +0100

    refactor: Create a `describe` block for every instruction

--- machine/index.spec.js
+++ machine/index.spec.js
@@ -27,118 +27,136 @@ describe('Virtual Machine', () => {
   });
 
   describe('Provided basic instructions', () => {
-    it('`LdaZero` loads 0 into accumulator', () => {
-      const instructions = [['LdaZero']];
-      const result = execute(instructions).inspect();
+    describe('LdaZero', () => {
+      it('loads 0 into accumulator', () => {
+        const instructions = [['LdaZero']];
+        const result = execute(instructions).inspect();
 
-      expect(result).toHaveProperty('registers.accumulator', 0);
+        expect(result).toHaveProperty('registers.accumulator', 0);
+      });
     });
 
-    it('`LdaSmi [x]` loads small int into accumulator', () => {
-      const instructions = [['LdaSmi', [6]]];
-      const result = execute(instructions).inspect();
+    describe('LdaSmi [imm]', () => {
+      it('loads small int into accumulator', () => {
+        const instructions = [['LdaSmi', [6]]];
+        const result = execute(instructions).inspect();
 
-      expect(result).toHaveProperty('registers.accumulator', 6);
+        expect(result).toHaveProperty('registers.accumulator', 6);
+      });
     });
 
-    it('`LdaGlobal [name_index] [feedback_slot_index]` loads global[constants[name_index]] into accumulator', () => {
-      const instructions = [['LdaGlobal', [0], [1]]];
-      const result = execute(instructions).inspect();
+    describe('LdaGlobal [name_index] [feedback_slot_index]', () => {
+      it('loads global[constants[name_index]] into accumulator', () => {
+        const instructions = [['LdaGlobal', [0], [1]]];
+        const result = execute(instructions).inspect();
 
-      expect(result).toHaveProperty('registers.accumulator', global.console);
+        expect(result).toHaveProperty('registers.accumulator', global.console);
+      });
     });
 
-    it('`LdaNamedProperty r [const_index] [y]` loads r[constants[const_index]] into accumulator', () => {
-      const instructions = [
-        ['LdaGlobal', [0], [1]],
-        ['Star1'],
-        ['LdaNamedProperty', 'r1', [1], [3]],
-      ];
+    describe('LdaNamedProperty reg [const_index [_]', () => {
+      it('`loads reg[constants[const_index]] into accumulator', () => {
+        const instructions = [
+          ['LdaGlobal', [0], [1]],
+          ['Star1'],
+          ['LdaNamedProperty', 'r1', [1], [3]],
+        ];
 
-      const result = execute(instructions).inspect();
+        const result = execute(instructions).inspect();
 
-      expect(result).toHaveProperty(
-        'registers.accumulator',
-        global.console.log
-      );
+        expect(result).toHaveProperty(
+          'registers.accumulator',
+          global.console.log
+        );
+      });
     });
 
-    it('`LdaConstant [const_index]` loads constants[const_index] into accumulator', () => {
-      const instructions = [['LdaConstant', [2]]];
-      const result = execute(instructions).inspect();
+    describe('LdaConstant [const_index]', () => {
+      it('loads constants[const_index] into accumulator', () => {
+        const instructions = [['LdaConstant', [2]]];
+        const result = execute(instructions).inspect();
 
-      expect(result).toHaveProperty('registers.accumulator', constants[2]);
+        expect(result).toHaveProperty('registers.accumulator', constants[2]);
+      });
     });
 
-    it.each([
+    describe.each([
       ['Star0', 'r0'],
       ['Star1', 'r1'],
       ['Star2', 'r2'],
-    ])("`%s` loads accumulator's value into register %s", (op, reg) => {
-      const instructions = [['LdaZero'], [op]];
-      const result = execute(instructions).inspect();
-
-      expect(result).toHaveProperty(`registers.${reg}`, 0);
-    });
-
-    it.each([
-      [true, -6],
-      [false, 0],
-      [false, 6],
-    ])(
-      '`TestLessThan [reg] [_]` sets flags.boolean=%s (reg<accumulator) with reg=%s and accumulator=0)',
-      (flag, value) => {
-        const instructions = [
-          ['LdaSmi', [value]],
-          ['Star0'],
-          ['LdaZero'],
-          ['TestLessThan', 'r0', [0]],
-        ];
+    ])('%s', (instruction, register) => {
+      it(`loads accumulator's value into register ${register}`, () => {
+        const instructions = [['LdaZero'], [instruction]];
         const result = execute(instructions).inspect();
 
-        expect(result).toHaveProperty(`flags.boolean`, flag);
-      }
-    );
-
-    it('`CallProperty1 r(prop) r(this) r(arg) [x]` calls fn with single value', () => {
-      let consoleLogMock = jest
-        .spyOn(global.console, 'log')
-        .mockImplementation();
-
-      const instructions = [
-        // r0 = console
-        ['LdaGlobal', [0], [1]],
-        ['Star0'],
-
-        // r1 = console.log
-        ['LdaNamedProperty', 'r0', [1], [3]],
-        ['Star1'],
-
-        // r2 = 'Less than zero'
-        ['LdaConstant', [2]],
-        ['Star2'],
-
-        // accumulator = console.log.call(console, 'Less than zero')
-        ['CallProperty1', 'r1', 'r0', 'r2', [5]],
-      ];
-
-      const result = execute(instructions).inspect();
-
-      expect(result).toHaveProperty('registers.accumulator', undefined);
-      expect(consoleLogMock).toHaveBeenCalledWith(constants[2]);
-
-      consoleLogMock.mockRestore();
+        expect(result).toHaveProperty(`registers.${register}`, 0);
+      });
     });
 
-    it('`Return` sets the return value to the value from the accumulator', () => {
-      const instructions = [['LdaZero'], ['Return']];
-      const result = execute(instructions).inspect();
+    describe('TestLessThan reg [_]', () => {
+      it.each([
+        [true, -6, 'lower than'],
+        [false, 0, 'equal to'],
+        [false, 6, 'bigger than'],
+      ])(
+        "sets flags.boolean to %s when register's value (%s) is %s accumulator's value (0)",
+        (flag, value, _) => {
+          const instructions = [
+            ['LdaSmi', [value]],
+            ['Star0'],
+            ['LdaZero'],
+            ['TestLessThan', 'r0', [0]],
+          ];
+          const result = execute(instructions).inspect();
 
-      expect(result).toHaveProperty(`return`, 0);
+          expect(result).toHaveProperty(`flags.boolean`, flag);
+        }
+      );
     });
 
-    it.todo('`JumpIfFalse [addr]`');
-    it.todo('`Ldar a`');
-    it.todo('`MulSmi [x] [y]`');
+    describe('CallProperty1 reg(prop) reg(obj) reg(arg) [_]', () => {
+      it('`calls obj[prop](arg) with single value', () => {
+        let consoleLogMock = jest
+          .spyOn(global.console, 'log')
+          .mockImplementation();
+
+        const instructions = [
+          // r0 = console
+          ['LdaGlobal', [0], [1]],
+          ['Star0'],
+
+          // r1 = console.log
+          ['LdaNamedProperty', 'r0', [1], [3]],
+          ['Star1'],
+
+          // r2 = 'Less than zero'
+          ['LdaConstant', [2]],
+          ['Star2'],
+
+          // accumulator = console.log.call(console, 'Less than zero')
+          ['CallProperty1', 'r1', 'r0', 'r2', [5]],
+        ];
+
+        const result = execute(instructions).inspect();
+
+        expect(result).toHaveProperty('registers.accumulator', undefined);
+        expect(consoleLogMock).toHaveBeenCalledWith(constants[2]);
+
+        consoleLogMock.mockRestore();
+      });
+    });
+
+    describe('Return', () => {
+      it('sets the return value to the value of the accumulator', () => {
+        const instructions = [['LdaZero'], ['Return']];
+        const result = execute(instructions).inspect();
+
+        expect(result).toHaveProperty(`return`, 0);
+      });
+    });
+
+    test.todo('`JumpIfFalse [addr]`');
+    test.todo('`Ldar a`');
+    test.todo('`MulSmi [x] [y]`');
   });
 });
