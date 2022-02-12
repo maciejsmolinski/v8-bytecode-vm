7afa2fa5d1736a21abde3ec1e686fac4866eb005
Maciej Smolinski <contact@maciejsmolinski.com>
2022-02-12 01:52:10 +0100

    feat: Spawn a mini-repl upon `Debugger` instruction

--- src/machine/execute.js
+++ src/machine/execute.js
@@ -1,4 +1,40 @@
 const { logger, registers: r } = require('../utils');
+const fs = require('fs');
+
+let buffer;
+
+function read() {
+  buffer = Buffer.alloc(1024);
+
+  const stdin = fs.openSync('/dev/stdin', 'rs');
+  const stdout = fs.openSync('/dev/stdout', 'as');
+
+  fs.writeSync(stdout, '*vm> ');
+  fs.readSync(stdin, buffer, 0, 1024, null);
+
+  fs.closeSync(stdin);
+  fs.closeSync(stdout);
+
+  return buffer.toString('utf8').replace(/\0/g, '').trim();
+}
+
+function repl(machine) {
+  const expression = read();
+
+  if (expression === 'quit') {
+    return;
+  }
+
+  try {
+    const fn = new Function('machine', `return ${expression}`);
+    console.log(fn(machine));
+  } catch (e) {
+    console.log(
+      `\x1b[31mVM: Error evaluating expression "${expression}"\x1b[0m`
+    );
+  }
+  repl(machine);
+}
 
 module.exports = function execute(machine, instructions) {
   machine.ip.set(0);
@@ -217,6 +253,10 @@ module.exports = function execute(machine, instructions) {
 
         machine.ip.set(address);
         continue;
+      }
+      case 'Debugger': {
+        repl(machine);
+        break;
       }
       default:
         break;
